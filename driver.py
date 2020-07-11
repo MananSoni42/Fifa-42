63294585307bd8e202ba4316a8112c625f24600f
Manan Soni <manansoni1399@gmail.comclear>
2020-07-11 23:11:37 +0530

    Add settings sub-menu, add colors for teams

--- driver.py
+++ driver.py
@@ -1,7 +1,6 @@
 from team import HumanTeam, RandomTeam
 from game import Game
 from settings import *
-from const import choose_formation
 import time
 import pygame_menu
 
@@ -21,9 +20,8 @@ def set_difficulty(value, difficulty):
 team1 = HumanTeam(formation='default', color=(0,32,255))
 team2 = RandomTeam(color=(255,128,0))
 
-game = Game(team1,team2)
-
 def play_game():
+    game = Game(team1,team2) # initialize the game
     """ Game loop """
     while not game.end: # Game loop
         clock.tick(FPS) # FPS
@@ -40,55 +38,23 @@ def play_game():
         pygame.display.update() # refresh screen
     pygame.quit()
 
-### Configuring the menu
+### The menu
+from menu import main_menu,instr_menu,about_menu,sett_menu,s1,s2
 
-# Custom theme
-custom_theme = pygame_menu.themes.THEME_DARK.copy() # define custom theme
+def color_change(widget, col, team): # Change color of widget and corresponding team
+    if col:
+        widget.set_background_color(col)
+        team.color = col
 
-menu_bg = pygame_menu.baseimage.BaseImage( # load background image
-    image_path=MENU_BG,
-    drawing_mode=pygame_menu.baseimage.IMAGE_MODE_FILL
-)
-custom_theme.background_color = menu_bg # Add background image
+s1.change = lambda col: color_change(s1, col, team1) # set team 1's color
+s2.change = lambda col: color_change(s2, col, team2) # Set team 2's color
 
-main_menu = pygame_menu.Menu(H, 3*W//4, 'FIFA-42',
-                             theme=custom_theme,
-                             mouse_motion_selection=True,
-                             mouse_visible=True)
+main_menu.add_button('Play', play_game)
+main_menu.add_button('Instructions', instr_menu)
+main_menu.add_button('Settings', sett_menu)
+main_menu.add_button('About', about_menu)
+main_menu.add_button('Quit', pygame_menu.events.EXIT) # Add exit button
 
-about_menu = pygame_menu.Menu(H, 3*W//4, 'About',
-                             theme=custom_theme,
-                             mouse_motion_selection=True,
-                             mouse_visible=True)
+###########################################
 
-instr_menu = pygame_menu.Menu(H, 3*W//4, 'Instructions',
-                             theme=custom_theme,
-                             mouse_motion_selection=True,
-                             mouse_visible=True)
-
-about_menu.add_label('This is the first game that we created. It was originally written in ANSI C++ and worked on console only.', max_char=70, font_size=FONT_SIZE//2)
-about_menu.add_label('It is a 9v9 Football game where you compete against the computer.', max_char=70, font_size=FONT_SIZE//2)
-about_menu.add_label('This game is written by:', max_char=60, font_size=FONT_SIZE//2)
-about_menu.add_label('Ashutosh Jani', max_char=60, font_size=FONT_SIZE//2)
-about_menu.add_label('Manan Soni', max_char=60, font_size=FONT_SIZE//2)
-about_menu.add_label('To know more visit github.com/MananSoni42/Fifa-42 ', max_char=60, font_size=FONT_SIZE//2)
-
-instr_menu.add_label('* Use arrow keys to move your player. ', max_char=60, font_size=FONT_SIZE//2)
-instr_menu.add_label('* Use the QWE, ASD, ZXC keys for passing the ball in an appropriate direction. ', max_char=60, font_size=FONT_SIZE//2)
-instr_menu.add_label('* In game, use the space key to toggle Position holding. ', max_char=60, font_size=FONT_SIZE//2)
-instr_menu.add_label('* (whether your players return to their original positions). ', max_char=60, font_size=FONT_SIZE//2)
-instr_menu.add_label('* Pause the game by pressing the esc key. ', max_char=60, font_size=FONT_SIZE//2)
-instr_menu.add_label('* Once in the pause menu, formations can be changed using. ', max_char=60, font_size=FONT_SIZE//2)
-instr_menu.add_label('* the space key followed by the left/right arrow keys. ', max_char=60, font_size=FONT_SIZE//2)
-instr_menu.add_label('* Use backspace to exit the pause menu. ', max_char=60, font_size=FONT_SIZE//2)
-
-about_menu.add_button('Back', pygame_menu.events.BACK)
-instr_menu.add_button('Back', pygame_menu.events.BACK)
-
-main_menu.add_button('Play', play_game) # Add play button to menu
-main_menu.add_button('Instructions', instr_menu) # Add set formation button to menu
-main_menu.add_button('About', about_menu) # Add set formation button to menu
-main_menu.add_button('Quit', pygame_menu.events.EXIT) #
-
-# Display the menu
-main_menu.mainloop(win)
+main_menu.mainloop(win) # Show the menu
