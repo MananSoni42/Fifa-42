daba2f4af1a121b05df3e2e35b445faa1c4dba22
Manan Soni <manansoni1399@gmail.com>
2021-01-03 12:25:33 +0530

    Make paths os independant

--- src/create_exe.py
+++ src/create_exe.py
@@ -1,7 +1,13 @@
-from cx_Freeze import setup,Executable
+import os
+os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
+
 import sys
+from cx_Freeze import setup,Executable
+from pathlib import Path
 import pygame
 import pygame_menu
+
+osname = 'linux'
 
 includefiles = [("teams","teams"),
     ("assets/fonts","assets/fonts"),
@@ -14,6 +20,8 @@ includes = ['args', 'ball', 'camera', 'const',
 excludes = ['PyInstaller']
 packages = ['pygame', 'pygame_menu', 'screeninfo']
 
+build_path = os.path.join(Path(__file__).resolve().parent, 'exec', osname)
+
 setup(
     name = 'Fifa-42',
     version = '1.1',
@@ -22,10 +30,10 @@ setup(
     author_email = 'manansoni1399@gmail.com',
     options = {'build_exe': {'includes': includes,
                              'excludes': excludes,
-                             'path': sys.path + ['../src'],
+                             'path': sys.path,
                              'packages': packages,
                              'include_files':includefiles,
-                             'build_exe': '../exec/linux'},
+                             'build_exe': build_path},
     },
     executables = [Executable('play.py', targetName='Fifa-42')]
 )
