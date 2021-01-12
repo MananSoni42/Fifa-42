aefc93204fea3a6bb91a013e8349b1397597c738
karnavDesai99 <45823615+karnavDesai99@users.noreply.github.com>
2021-01-12 15:29:36 +0530

    Drew border

--- web/game.js
+++ web/game.js
@@ -1,20 +1,24 @@
 canvas = document.getElementById("canvas");
-canvas.width = screen.width;
-canvas.height = screen.height;
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
 var W = canvas.width;
 var H = canvas.height;
 
 var ctx = canvas.getContext("2d");
 
+ctx.lineWidth=2;
+
 function field_draw() {
     ctx.fillStyle = "green";
     ctx.fillRect(0, 0, W, H);
-
     ctx.strokeStyle = "white";
+    
     ctx.beginPath();
     ctx.arc(W/2, H/2, H/10, 0, 2 * Math.PI, false);
     ctx.stroke();
-    cts.closePath();
+    ctx.closePath();
+    ctx.strokeRect(0, 0, W-ctx.lineWidth, H-ctx.lineWidth);
+    ctx.strokeRect(0, 0, W-ctx.lineWidth, H-ctx.lineWidth);
 }
 
 field_draw();
