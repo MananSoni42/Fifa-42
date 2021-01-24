a6804443409e6f46039ee397eea15325d4cf5a1e
karnavDesai99 <karnavdesai2000@hotmail.com>
2021-01-24 17:48:39 +0530

    State is being displayed

--- web/game.js
+++ web/game.js
@@ -1,33 +1,37 @@
-var state = {'ball': (683, 384),
- 'team1': {'goal_x': 0,
-           'players': [{ "id": 0, "pos": (33, 384), "img": "L0" },
-                       { "id": 1, "pos": (341, 308), "img": "L0" },
-                       { "id": 2, "pos": (341, 460), "img": "L0" },
-                       { "id": 3, "pos": (426, 109), "img": "L0" },
-                       { "id": 4, "pos": (426, 658), "img": "L0" },
-                       { "id": 5, "pos": (683, 308), "img": "L0" },
-                       { "id": 6, "pos": (683, 460), "img": "L0" },
-                       { "id": 7, "pos": (768, 109), "img": "L0" },
-                       { "id": 8, "pos": (768, 658), "img": "L0" },
-                       { "id": 9, "pos": (1109, 231), "img": "L0" },
-                       { "id": 10, "pos": (1109, 537), "img": "L0" }]},
+var state = {
+  'W': 1920,
+  'H': 1080,
+  'ball': [683, 384],
+  'team1': {'goal_x': 0,
+           'players': [{ "id": 0, "pos": [33, 384], "img": "L0" },
+                       { "id": 1, "pos": [341, 308], "img": "L0" },
+                       { "id": 2, "pos": [341, 460], "img": "L0" },
+                       { "id": 3, "pos": [426, 109], "img": "L0" },
+                       { "id": 4, "pos": [426, 658], "img": "L0" },
+                       { "id": 5, "pos": [683, 308], "img": "L0" },
+                       { "id": 6, "pos": [683, 460], "img": "L0" },
+                       { "id": 7, "pos": [768, 109], "img": "L0" },
+                       { "id": 8, "pos": [768, 658], "img": "L0" },
+                       { "id": 9, "pos": [1109, 231], "img": "L0" },
+                       { "id": 10, "pos": [1109, 537], "img": "L0" }]},
  'team2': {'goal_x': 1366,
-           'players': [{ "id": 0, "pos": (1333, 384), "img": "L0" },
-                       { "id": 1, "pos": (1025, 460), "img": "L0" },
-                       { "id": 2, "pos": (1025, 308), "img": "L0" },
-                       { "id": 3, "pos": (940, 659), "img": "L0" },
-                       { "id": 4, "pos": (940, 110), "img": "L0" },
-                       { "id": 5, "pos": (656, 576), "img": "L0" },
-                       { "id": 6, "pos": (656, 192), "img": "L0" },
-                       { "id": 7, "pos": (792, 384), "img": "L0" },
-                       { "id": 8, "pos": (520, 384), "img": "L0" },
-                       { "id": 9, "pos": (257, 537), "img": "L0" },
-                       { "id": 10, "pos": (257, 231), "img": "L0" }]}}
+           'players': [{ "id": 0, "pos": [1333, 384], "img": "L0" },
+                       { "id": 1, "pos": [1025, 460], "img": "L0" },
+                       { "id": 2, "pos": [1025, 308], "img": "L0" },
+                       { "id": 3, "pos": [940, 659], "img": "L0" },
+                       { "id": 4, "pos": [940, 110], "img": "L0" },
+                       { "id": 5, "pos": [656, 576], "img": "L0" },
+                       { "id": 6, "pos": [656, 192], "img": "L0" },
+                       { "id": 7, "pos": [792, 384], "img": "L0" },
+                       { "id": 8, "pos": [520, 384], "img": "L0" },
+                       { "id": 9, "pos": [257, 537], "img": "L0" },
+                       { "id": 10, "pos": [257, 231], "img": "L0" }]}};
 
 
 canvas = document.getElementById("canvas");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
+
 var W = canvas.width;
 var H = canvas.height;
 
@@ -36,12 +40,13 @@ var ctx = canvas.getContext("2d");
 ctx.lineWidth=2;
 
 var BALL_RADIUS=3;
+var PLAYER_RADIUS=15;
 
 function field_draw() {
     ctx.fillStyle = "green";
     ctx.fillRect(0, 0, W, H);
     ctx.strokeStyle = "white";
-    
+
     ctx.beginPath();
     ctx.arc(W/2, H/2, H/7, 0, 2 * Math.PI, false); // center circle
     ctx.stroke();
@@ -53,15 +58,33 @@ function field_draw() {
     ctx.strokeRect(ctx.lineWidth/2,0.2*H, 0.1*W, 0.6*H) // left penalty area
 }
 
-function draw_ball(state) {
-    "use strict";
-    var img = document.getElementById("ball");
-    ctx.drawImage(img,50,50);
-} 
+function draw_ball(x,y,w,h) {
+    // "use strict";
+    // var img = document.getElementById("ball");
+    // ctx.drawImage(img,5,5);
+    console.log("drawing at" + x + " " + y);
+    ctx.beginPath();
+    ctx.arc(Math.round(x*W/w), Math.round(y*H/h), BALL_RADIUS, 0, 2 * Math.PI, true);
+    ctx.stroke();
+    ctx.closePath();
+    ctx.fillStyle = "black";
+    ctx.fill();
+}
 
-//function draw_player()
+function draw_player(x,y,w,h,color) {
+    ctx.beginPath();
+    ctx.rect(Math.round((x-PLAYER_RADIUS)*W/w),Math.round((y-PLAYER_RADIUS)*H/h),2*PLAYER_RADIUS,2*PLAYER_RADIUS);
+    ctx.fillStyle=color;
+    ctx.fill();
+    ctx.closePath();
+}
 
 $(document).ready(() => {
     field_draw();
-    draw_ball(state);
-})
+    console.log(state["ball"]);
+    draw_ball(state["ball"][0], state["ball"][1], state['W'], state['H']);
+    for(i=0;i<11;i++) {
+      draw_player(state["team1"]["players"][i]["pos"][0], state["team1"]["players"][i]["pos"][1], state['W'], state['H'], "red");
+      draw_player(state["team2"]["players"][i]["pos"][0], state["team2"]["players"][i]["pos"][1], state['H'], state['H'], "blue");
+    }
+});
